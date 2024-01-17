1444335a92fb015050049d2c9cbe83b274df49dd
Piotr Poterek <peterpoterek0@gmail.com>
2024-01-17 16:59:13 +0100

    Notification conditional rendering

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,15 +1,21 @@
+import Notification from '../Notification/Notification.jsx';
+
 const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
   return (
     <>
       <h2>Statistics</h2>
 
-      <ul>
-        <li>Good: {good}</li>
-        <li>Neutral: {neutral}</li>
-        <li>Bad: {bad}</li>
-        <li>Total: {total}</li>
-        <li>Positive Feedback: {positivePercentage}%</li>
-      </ul>
+      {total === 0 ? (
+        <Notification message={'There is no feedback'} />
+      ) : (
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+          <li>Total: {total}</li>
+          <li>Positive Feedback: {positivePercentage}%</li>
+        </ul>
+      )}
     </>
   );
 };
